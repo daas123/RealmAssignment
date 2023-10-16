cef4e88c40801f59f5d17e38cee7ef962f0436ed
saad <saadneosoft@gmail.com>
2023-10-16 12:11:01 +0530

    Initial Commit

---
+++ RealmAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmAssignment
+//
+//  Created by Neosoft on 16/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
